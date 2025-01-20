73a515abefde0c185962d5a40e958a9ebf8d2ef1
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-01-20 15:18:12 +0100

    fix: logging and division

--- src/szamologep.js
+++ src/szamologep.js
@@ -1,4 +1,4 @@
 export const osszead = (a, b) => a + b;
 export const kivon = (a, b) => a - b;
 export const szoroz = (a, b) => a * b;
-export const eloszt = (a, b) => Math.round(a / b);
+export const eloszt = (a, b) => Math.floor(a / b);
